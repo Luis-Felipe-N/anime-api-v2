8facc3a6ed7e8a98d8fe087d6b110edd6ae3ff2d
luis.nunes <luisj2felipe09@gmail.com>
2024-03-17 14:04:37 -0300

    :goal_net: Update either ts

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,22 +1,38 @@
 // Error
-export class Failure<F> {
+export class Failure<F, S> {
   readonly value: F
 
   constructor(value: F) {
     this.value = value
   }
+
+  isSuccess(): this is Success<F, S> {
+    return false
+  }
+
+  isFailure(): this is Failure<F, S> {
+    return true
+  }
 }
 
 // Success
-export class Success<S> {
+export class Success<F, S> {
   readonly value: S
 
   constructor(value: S) {
     this.value = value
   }
+
+  isSuccess(): this is Success<F, S> {
+    return false
+  }
+
+  isFailure(): this is Failure<F, S> {
+    return true
+  }
 }
 
-export type Either<F, S> = Failure<F> | Success<S>
+export type Either<F, S> = Failure<F, S> | Success<F, S>
 
 export const failure = <F, S>(value: F): Either<F, S> => {
   return new Failure(value)
