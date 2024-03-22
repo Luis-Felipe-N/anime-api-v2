cb8226fdc078a4b5d0bdd45bbf150778867d209c
luis.nunes <luisj2felipe09@gmail.com>
2024-03-22 19:01:19 -0300

    :sparkles: Factories to use controllers

--- src/infra/factories/make-fetch-animes-by-genre-use-case.ts
+++ src/infra/factories/make-fetch-animes-by-genre-use-case.ts
@@ -1,16 +1,18 @@
 import { PrismaAnimesRepository } from '../database/repositories/prisma-animes-repository'
-import { UploadAnimeBySlugUseCase } from '@/domain/application/use-cases/upload-anime-by-slug'
 import { PrismaSeasonsRepository } from '../database/repositories/prisma-seasons-repository'
+import { FetchAnimesByGenreUseCase } from '@/domain/application/use-cases/fetch-animes-by-genre'
 import { PrismaGenresRepository } from '../database/repositories/prisma-genres-repository'
 
-export function makeUploadAnimeBySlugUseCase() {
+export function makeFetchAnimesByGenreUseCase() {
   const seasonsRepository = new PrismaSeasonsRepository()
   const genresRepository = new PrismaGenresRepository()
   const animesRepository = new PrismaAnimesRepository(
     seasonsRepository,
     genresRepository,
   )
-  const useCase = new UploadAnimeBySlugUseCase(animesRepository)
-
+  const useCase = new FetchAnimesByGenreUseCase(
+    animesRepository,
+    genresRepository,
+  )
   return useCase
 }
