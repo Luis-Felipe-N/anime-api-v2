2225f8f4d704d45d8a222fb3ec6da120fee6fa31
luis.nunes <luisj2felipe09@gmail.com>
2024-03-24 15:52:45 -0300

    :sparkles: Get episode by id use-case

---
+++ src/domain/application/use-cases/get-episode-by-id.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryEpisodesRepository } from 'test/repositories/in-memory-episodes-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { makeEpisode } from 'test/factories/make-episode'
+import { GetEpisodeByIdUseCase } from './get-episode-by-id'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryEpisodesRepository: InMemoryEpisodesRepository
+let sut: GetEpisodeByIdUseCase
+
+describe('Get Episode by id', () => {
+  beforeEach(() => {
+    inMemoryEpisodesRepository = new InMemoryEpisodesRepository()
+    sut = new GetEpisodeByIdUseCase(inMemoryEpisodesRepository)
+  })
+
+  it('should be able to get episode by slug', async () => {
+    const newEpisode = makeEpisode({}, new UniqueEntityId('id-do-episodio'))
+
+    await inMemoryEpisodesRepository.create(newEpisode)
+
+    const result = await sut.execute({
+      id: 'id-do-episodio',
+    })
+
+    expect(result.isSuccess()).toBe(true)
+  })
+
+  it('should not be able to get episode with non-exists id', async () => {
+    const result = await sut.execute({
+      id: 'non-exists-id',
+    })
+
+    expect(result.isFailure()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+})
---
+++ src/domain/application/use-cases/get-episode-by-id.ts
@@ -0,0 +1,31 @@
+import { Episode } from '@/domain/enterprise/entities/episode'
+import { EpisodesRepository } from '../repositories/episode.repository'
+import { Either, failure, success } from '@/core/either'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetEpisodeByIdUseCaseRequest {
+  id: string
+}
+
+type GetEpisodeByIdUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    episode: Episode
+  }
+>
+
+export class GetEpisodeByIdUseCase {
+  constructor(private episodesRepository: EpisodesRepository) {}
+
+  async execute({
+    id,
+  }: GetEpisodeByIdUseCaseRequest): Promise<GetEpisodeByIdUseCaseResponse> {
+    const episode = await this.episodesRepository.findById(id)
+
+    if (!episode) {
+      return failure(new ResourceNotFoundError())
+    }
+
+    return success({ episode })
+  }
+}
