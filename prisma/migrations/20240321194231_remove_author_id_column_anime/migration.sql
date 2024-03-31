31decee4783389fd4bdaf1263a2622e152abd685
luis.nunes <luisj2felipe09@gmail.com>
2024-03-30 21:50:18 -0300

    :card_file_box: Rating anime column

--- prisma/migrations/20240321194231_remove_author_id_column_anime/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropForeignKey
-ALTER TABLE "animes" DROP CONSTRAINT "animes_author_id_fkey";
--- prisma/migrations/20240325220714_reset_migrations/migration.sql
+++ prisma/migrations/20240325220714_reset_migrations/migration.sql
@@ -25,7 +25,6 @@ CREATE TABLE "animes" (
     "cover" TEXT,
     "nsfw" BOOLEAN NOT NULL DEFAULT false,
     "trailerYtId" TEXT,
-    "author_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
 
@@ -37,7 +36,7 @@ CREATE TABLE "seasons" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "anime_id" TEXT,
+    "anime_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
 
@@ -54,11 +53,22 @@ CREATE TABLE "episodes" (
     "cover" TEXT NOT NULL,
     "duration" DOUBLE PRECISION NOT NULL,
     "index" INTEGER NOT NULL,
+    "video" TEXT NOT NULL,
     "link-embed" TEXT,
-    "season_id" TEXT,
+    "season_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "episodes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "genres" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "anime_id" TEXT NOT NULL,
+
+    CONSTRAINT "genres_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -77,7 +87,7 @@ CREATE TABLE "comments" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "author_id" TEXT NOT NULL,
-    "episode_id" TEXT,
+    "episode_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
 
@@ -96,14 +106,17 @@ CREATE UNIQUE INDEX "seasons_slug_key" ON "seasons"("slug");
 -- CreateIndex
 CREATE UNIQUE INDEX "episodes_slug_key" ON "episodes"("slug");
 
--- AddForeignKey
-ALTER TABLE "animes" ADD CONSTRAINT "animes_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "genres_slug_key" ON "genres"("slug");
 
 -- AddForeignKey
-ALTER TABLE "seasons" ADD CONSTRAINT "seasons_anime_id_fkey" FOREIGN KEY ("anime_id") REFERENCES "animes"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "seasons" ADD CONSTRAINT "seasons_anime_id_fkey" FOREIGN KEY ("anime_id") REFERENCES "animes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "episodes" ADD CONSTRAINT "episodes_season_id_fkey" FOREIGN KEY ("season_id") REFERENCES "seasons"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "episodes" ADD CONSTRAINT "episodes_season_id_fkey" FOREIGN KEY ("season_id") REFERENCES "seasons"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "genres" ADD CONSTRAINT "genres_anime_id_fkey" FOREIGN KEY ("anime_id") REFERENCES "animes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "watched" ADD CONSTRAINT "watched_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -112,4 +125,4 @@ ALTER TABLE "watched" ADD CONSTRAINT "watched_user_id_fkey" FOREIGN KEY ("user_i
 ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_episode_id_fkey" FOREIGN KEY ("episode_id") REFERENCES "episodes"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "comments" ADD CONSTRAINT "comments_episode_id_fkey" FOREIGN KEY ("episode_id") REFERENCES "episodes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
