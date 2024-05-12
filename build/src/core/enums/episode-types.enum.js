26e42e1d01168ddfaf8b10344f24e6bd653428a3
luisnunes <luisj2felipe09@gmail.com>
2024-05-12 12:15:22 -0300

    🔇 Remove build path

--- build/src/core/enums/episode-types.enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EpisodeTypes = void 0;
-exports.EpisodeTypes = {
-    CRUNCHYROLL: 'CRUNCHYROLL',
-    ANIMESONLINE: 'ANIMESONLINE',
-};
--- build/src/domain/application/use-cases/errors/user-already-exists-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserAlreadyExistsError = void 0;
-class UserAlreadyExistsError extends Error {
-    constructor() {
-        super('User with e-email already exits');
-    }
-}
-exports.UserAlreadyExistsError = UserAlreadyExistsError;
